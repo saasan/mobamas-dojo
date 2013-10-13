1faf5ae8335b578ad9bc7dea7566aeacce0c0ad9
saasan <saasan.s2works@hotmail.com>
2013-10-14 01:52:44 +0900

    webtoolkit.base64.js追加

--- compile.bat
+++ compile.bat
@@ -2,7 +2,7 @@
 PUSHD %~dp0
 
 PUSHD js
-java -jar compiler.jar --compilation_level SIMPLE_OPTIMIZATIONS --js storage.js --js config.js --js mobamas-dojo.js --js init.js --js_output_file mobamas-dojo-min.js
+java -jar compiler.jar --compilation_level SIMPLE_OPTIMIZATIONS --js webtoolkit.base64.js --js storage.js --js config.js --js mobamas-dojo.js --js init.js --js_output_file mobamas-dojo-min.js
 POPD
 
 PUSHD css
