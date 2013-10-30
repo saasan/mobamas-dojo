ed2186ec5e7fad4950c11a8e52093bef5c740443
saasan <saasan.s2works@hotmail.com>
2013-10-31 06:54:22 +0900

    compass導入

--- compile.bat
+++ compile.bat
@@ -1,12 +1,10 @@
 %~d0
 PUSHD %~dp0
 
+call compass compile -s compressed
+
 PUSHD js
 java -jar compiler.jar --compilation_level SIMPLE_OPTIMIZATIONS --js storage.js --js config.js --js birthday.js --js mobamas-dojo.js --js init.js --js_output_file mobamas-dojo-min.js
-POPD
-
-PUSHD css
-call sass --style compressed mobamas-dojo.scss:mobamas-dojo-min.css
 POPD
 
 POPD
