4e4d2198707c9893b2a6fa5af6c8468dc5ac6361
saasan <saasan.s2works@hotmail.com>
2013-11-05 22:05:29 +0900

    デバッグ時はCSSを圧縮しない

--- compile.bat
+++ compile.bat
@@ -1,7 +1,12 @@
+@ECHO OFF
 %~d0
 PUSHD %~dp0
 
-call compass compile -s compressed
+IF "%1" EQU "debug" (
+  CALL compass compile -e development --force
+) ELSE (
+  CALL compass compile -e production --force
+)
 
 PUSHD js
 java -jar compiler.jar --compilation_level SIMPLE_OPTIMIZATIONS --js storage.js --js config.js --js birthday.js --js mobamas-dojo.js --js init.js --js_output_file mobamas-dojo-min.js
--- syncDebug.bat
+++ syncDebug.bat
@@ -2,7 +2,7 @@
 %~d0
 PUSHD %~dp0
 
-CALL compile.bat
+CALL compile.bat debug
 python main.py --debug
 
 robocopy . Z:\htdocs\mobamas-dojo * /MIR /XD .git .sass-cache /XF .gitattributes .gitignore compiler.jar template.html *.bat *.py /XA:SH
