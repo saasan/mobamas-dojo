d20e040e75686ceee18abb69acc382e4ee7612e0
saasan <saasan.s2works@hotmail.com>
2013-11-20 09:52:35 +0900

    FlexSlider

--- compile.bat
+++ compile.bat
@@ -9,7 +9,7 @@ IF "%1" EQU "debug" (
 )
 
 PUSHD js
-java -jar compiler.jar --compilation_level SIMPLE_OPTIMIZATIONS --js toast.js --js storage.js --js config.js --js birthday.js --js mobamas-dojo.js --js init.js --js_output_file mobamas-dojo-min.js
+java -jar compiler.jar --compilation_level SIMPLE_OPTIMIZATIONS --js flexslider\jquery.flexslider-min.js --js toast.js --js storage.js --js config.js --js birthday.js --js mobamas-dojo.js --js init.js --js_output_file mobamas-dojo-min.js
 POPD
 
 POPD
