57703e2d876a1dfadb4b999e1c65552f8ccb92f3
saasan <saasan.s2works@hotmail.com>
2014-08-02 17:49:00 +0900

    buildAndSyncRelease.batの対象外ファイルを再修正

--- buildAndSyncRelease.bat
+++ buildAndSyncRelease.bat
@@ -4,6 +4,6 @@ PUSHD %~dp0
 
 CALL buildRelease.bat
 
-robocopy . ..\saasan.github.com\mobamas-dojo * /MIR /XD .git .sass-cache /XF .gitattributes .gitignore compiler.jar buildAndSyncRelease.bat buildDebug.bat buildRelease.bat compile.bat /XA:SH
+robocopy . ..\saasan.github.com\mobamas-dojo * /MIR /XD .git .sass-cache sass /XF .gitattributes .gitignore compiler.jar buildAndSyncRelease.bat buildDebug.bat buildRelease.bat compile.bat config.rb /XA:SH
 
 POPD
